a9e6f1059a0b8c197c4387270ff61d949d3ba60a
Idehed anton <anton.idehed@telia.com>
2024-06-18 13:53:34 +0000

    added urls, serializers and updated views in profile

---
+++ profiles/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Profile
+
+
+class ProfileSerializers(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Profile
+        fields = [
+            'id', 'owner', 'created_at', 'updated_at', 'name',
+            'email', 'content', 'image', 'facebook_link', 'x_link', 
+            'instagram_link', 'youtube_link'
+        ]
