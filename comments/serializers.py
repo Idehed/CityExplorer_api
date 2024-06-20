06269a92921503ad6095f6b25aeaa5c725aa9294
Anton Idehed <anton.idehed@telia.com>
2024-06-20 13:46:51 +0000

    added commentlistview and commentdetailview

--- comments/serializers.py
+++ comments/serializers.py
@@ -12,11 +12,11 @@ class CommentSerializer(serializers.ModelSerializer):
         return request.user == obj.owner
 
     class Meta:
-        model = Post
+        model = Comment
         fields = [
             'id', 'owner', 'is_owner', 'profile_id',
-            'profile_image', 'post' 'created_at', 'updated_at',
-            'content',
+            'profile_image', 'post', 'created_at', 'updated_at',
+            'content'
         ]
 
 class CommentDetailSerializer(CommentSerializer):
