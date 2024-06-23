d13c5769eaa30601e8c014a5904543344cc2ea78
Anton Idehed <anton.idehed@telia.com>
2024-06-23 15:34:39 +0000

    added likes_count and comments_count for posts

--- profiles/views.py
+++ profiles/views.py
@@ -12,7 +12,7 @@ class ProfileList(generics.ListAPIView):
     """
     queryset = Profile.objects.annotate(
         posts_count=Count('owner__post', distinct=True),
-        followers_coun=Count('owner__followed',distinct=True),
+        followers_count=Count('owner__followed',distinct=True),
         following_count=Count('owner__following', distinct=True)
     ).order_by('-created_at')
     serializer_class = ProfileSerializer
