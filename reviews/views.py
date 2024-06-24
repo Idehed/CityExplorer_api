8fec7adfc7cc61d3ffa20ba671c30cabbc74ef5c
Anton Idehed <anton.idehed@telia.com>
2024-06-24 19:25:33 +0000

    created 2 classes in views.py and created urls.py

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from django_filters.rest_framework import DjangoFilterBackend
+from drf_api.permissions import IsOwnerOrReadOnly
+from .models import Review
+from .serializers import ReviewDetailSerializer, ReviewSerializer
 
-# Create your views here.
+class ReviewList(generics.ListCreateAPIView):
+    """
+    """
+    serializer_class = ReviewSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Review.objects.all()
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['guide']
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = ReviewDetailSerializer
+    queryset = Review.objects.all()
