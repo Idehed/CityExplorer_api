4c28bb105a3496f99fa863e5baeabd9072f7efcc
Anton Idehed <anton.idehed@telia.com>
2024-06-24 18:23:43 +0000

    created GuideList view

--- guides/views.py
+++ guides/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions, filters
+from drf_api.permissions import IsOwnerOrReadOnly
+from .models import Guide
+from .serializers import GuideSerializer
 
-# Create your views here.
+class GuideList(generics.ListCreateAPIView):
+    """
+    """
+    serializer_class = GuideSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Guide.objects.all()
+
+    filter_backends = [
+        filters.SearchFilter
+    ]
+
+    search_fields = [
+        'owner__username',
+        'city'
+    ]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
