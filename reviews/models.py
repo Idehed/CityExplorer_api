282d9601e11d806bb16ca69557694a638cd6fef6
Anton Idehed <anton.idehed@telia.com>
2024-06-24 18:52:00 +0000

    creating model for review

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from guides.models import Guide
 
-# Create your models here.
+class Review(models.Model):
+    """
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    guide = models.ForeignKey(
+        Guide, on_delete=models.CASCADE, related_name='reviews'
+    )
+    content = models.TextField()
+    rating = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"{self.owner}' review"
