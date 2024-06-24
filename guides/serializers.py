da15945fb31d25c4fc433a335748c9d64f6d592f
Anton Idehed <anton.idehed@telia.com>
2024-06-24 18:17:19 +0000

    created serializers.py

---
+++ guides/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from .models import Guide
+
+class GuideSerializer(serializers.ModelSerializer):
+    """
+    """
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+
+    class Meta:
+        """
+        """
+        model = Guide
+        fields = [
+            'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
+            'profile_image', 'city', 'duration', 'cost',
+            'email', 'phone', 'created_at', 'updated_at',
+        ]
