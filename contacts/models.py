49a39e10febadf5ae8ee26873f02d433f23bac93
Anton Idehed <anton.idehed@telia.com>
2024-06-25 18:39:36 +0000

    create model for contact form

--- contacts/models.py
+++ contacts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    """
+    Model to handle the contact form submissions
+    """
+
+    name = models.CharField(max_length=200)
+    topic = models.CharField(max_length=50)
+    email = models.EmailField()
+    message = models.TextField(max_length=700)
+    created_at = models.DateTimeField(auto_now_add=True)
+    read = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"From {self.name} : {self.topic}"
